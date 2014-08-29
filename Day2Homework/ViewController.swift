5f05eb7228da82ff0166d8b24bbc9f81df1cd44a
Dzung <Nguyen>
2014-08-29 11:09:08 +0700

    Initial Commit

---
+++ Day2Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day2Homework
+//
+//  Created by Dzung Nguyen on 8/29/14.
+//  Copyright (c) 2014 Dzung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
